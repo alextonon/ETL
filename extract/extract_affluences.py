487e57ccb0c6a610a23195b2b7599141344de3f4
octave.claich <octave.claich@gmail.com>
2025-10-12 15:57:46 +0200

    Partie transform affluences quasi finie

--- extract/extract_affluences.py
+++ extract/extract_affluences.py
@@ -4,6 +4,14 @@ import json
 import time
 
 def extract_data_insee(api_url):
+    """
+    Fonction qui effectue une requête sur l'API de l'INSEE, et renvoie le dataframe recherché.
+    Source : https://portail-api.insee.fr/catalog/api/a890b735-159c-4c91-90b7-35159c7c9126/doc
+    Args:
+        api_url (url): url de la requête que l'on veut effectuer
+    Returns:
+        df (pd.DataFrame): DataFrame python résultant de la requête
+    """
 
     try:
         print("Making API request... (this may take a few seconds)")
@@ -61,32 +69,76 @@ def extract_data_insee(api_url):
     
 
 def extract_data_capacite() :
+    """
+    Fonction qui extrait les données de capacités d'hébergements touristiques dans toute la France.
+    Ces données ont été actualisées en 2025, et pour chaque commune, on a accès au nombre de lits
+    proposés pour différentes catégories : hôtels, campings, ...
+    Pour les nombres de nuitées on a uniquement accès aux hôtels et aux campings donc on va faire
+    de même pour les capacités.
+    L'appel à l'API de l'INSEE ne permet d'exporter que 10000 lignes à la fois, or le dataframe
+    concerné (DS_TOUR_CAP) en comporte bien plus, on va donc requêter l'API de manière séparée
+    pour chaque département, et concaténer les résultats obtenus dans le dataframe qui sera retourné.
+    Args: 
+        None
+    Returns:
+        df_capacite (pd.DataFrame): DataFrame python contenant, pour chaque ville et chaque type
+    d'hébergement, le nombre de lits disponibles.
+    """
 
-    liste_id_regions = [11, 24, 27, 28, 32, 44, 52, 53, 75, 76, 84, 93, 94]
+    # Identifiants des départements dans la nomenclature INSEE
+    liste_id_departements = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+                            "11", "12", "13", "14", "15", "16", "17", "18", "19", "21",
+                            "22", "23", "24", "25", "26", "27", "28", "29", "2A", "2B",
+                            "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+                            "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+                            "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+                            "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+                            "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+                            "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+                            "90", "91", "92", "93", "94", "95"]
 
-    liste_api_url_capacite_region = []
+    liste_api_url_capacite_departement = []
 
-    for id_region in liste_id_regions :
+    # On construit une liste d'URLs pour chaque département
+    for id_departement in liste_id_departements :
 
-        liste_api_url_capacite_region.append(f"https://api.insee.fr/melodi/data/DS_TOUR_CAP?TOUR_MEASURE=PLACE&UNIT_LOC_RANKING=_T&L_STAY=_T&GEO=COM&GEO=2025-REG-{id_region}")
+        liste_api_url_capacite_departement.append(f"https://api.insee.fr/melodi/data/DS_TOUR_CAP?TOUR_MEASURE=PLACE&UNIT_LOC_RANKING=_T&L_STAY=_T&ACTIVITY=I551&ACTIVITY=I553&GEO=2025-DEP-{id_departement}*COM")
 
     df_capacite = pd.DataFrame()
 
-    for api_url_capacite_region in liste_api_url_capacite_region :
+    # Pour chaque département, on appelle l'URL correspondante pour remplir df_capacite
+    for i in range(len(liste_api_url_capacite_departement)) :
 
-        df_capacite_region = extract_data_insee(api_url_capacite_region)
+        df_capacite_region = extract_data_insee(liste_api_url_capacite_departement[i])
         df_capacite = pd.concat([df_capacite, df_capacite_region], ignore_index=True)
+        print(f"Département {liste_id_departements[i]} ajouté")
 
     return df_capacite
 
 
-def extract_data_taux_remplissage() :
+def extract_data_nb_nuitees() :
+    """
+    Fonction qui extrait les nombres de nuitées des différents types d'hébergements (hôtel,
+    camping, ...), pour chaque mois de l'année 2024, et pour l'année 2024 au global,
+    dans chaque département de France.
+    Au niveau du département, on a accès uniquement aux hôtels et aux campings, et pas aux autres
+    types d'hébergements malheureusement. De plus, les données mensuelles sont uniquement
+    disponibles pour les hôtels et pas pour les campings, donc on va estimer ces dernières en
+    récupérant les données annuelles et en répartissant selon l'importance des mois (cf plus tard).
+    A l'origine on aurait voulu récupérer les taux d'occupation plutôt, mais il manque des données
+    dans la source INSEE (on a uniquement les hôtels au niveau départemental), donc on a préféré
+    récupérer les nombres de nuitées.
+    Args:
+        None
+    Returns:
+        df_nb_nuitees (pd.DataFrame): DataFrame python contenant les informations souhaitées.
+    """
 
-    api_url_taux_remplissage = "https://api.insee.fr/melodi/data/DS_TOUR_FREQ?FREQ=M&TOUR_RESID=_T&HOTEL_STA=_T&TERRTYPO=_T&TOUR_MEASURE=PLACE_OCCUPANCY_RATE&UNIT_LOC_RANKING=_T&TIME_PERIOD=2024-01&TIME_PERIOD=2024-02&TIME_PERIOD=2024-03&TIME_PERIOD=2024-04&TIME_PERIOD=2024-05&TIME_PERIOD=2024-06&TIME_PERIOD=2024-07&TIME_PERIOD=2024-08&TIME_PERIOD=2024-09&TIME_PERIOD=2024-10&TIME_PERIOD=2024-11&TIME_PERIOD=2024-12&GEO=DEP"
+    api_url_nb_nuitees = "https://api.insee.fr/melodi/data/DS_TOUR_FREQ?FREQ=A&FREQ=M&TOUR_RESID=_T&HOTEL_STA=_T&TERRTYPO=_T&TOUR_MEASURE=NUI&UNIT_LOC_RANKING=_T&TIME_PERIOD=2024&TIME_PERIOD=2024-01&TIME_PERIOD=2024-02&TIME_PERIOD=2024-03&TIME_PERIOD=2024-04&TIME_PERIOD=2024-05&TIME_PERIOD=2024-06&TIME_PERIOD=2024-07&TIME_PERIOD=2024-08&TIME_PERIOD=2024-09&TIME_PERIOD=2024-10&TIME_PERIOD=2024-11&TIME_PERIOD=2024-12&GEO=DEP"
 
-    df_taux_remplissage = extract_data_insee(api_url_taux_remplissage)
+    df_nb_nuitees = extract_data_insee(api_url_nb_nuitees)
 
-    return df_taux_remplissage
+    return df_nb_nuitees
 
 
 
@@ -95,5 +147,5 @@ def extract_data_taux_remplissage() :
 if __name__ == '__main__' :
 
 
-    df_taux_remplissage = extract_data_taux_remplissage()
-    print(df_taux_remplissage.head())
+    df_capacite = extract_data_capacite()
+    print(len(df_capacite))
--- transform/transform_affluences.py
+++ transform/transform_affluences.py
@@ -3,11 +3,28 @@ import pandas as pd
 import extract.extract_affluences as extract_affluences
 
 def transform_data_capacite(df_capacite) :
+    """
+    Traitement du dataframe contenant les informations sur les capacités d'hébergement de chaque
+    commune. Les données obtenues sont encodées selon une nomenclature propre à l'INSEE, on va
+    donc nettoyer ces informations pour qu'elles apparaissent le plus clairement possible dans
+    le dataframe, et supprimer les colonnes inutiles.
+    Args:
+        df_capacite (pd.DataFrame): DataFrame résultant de l'appel à la fonction
+    extract.extract_affluences.extract_data_capacite.
+    Returns:
+        df_capacite (pd.DataFrame): DataFrame nettoyé.
+    """
 
+    # On enlève toutes les colonnes correspondant à des valeurs que l'on a déjà filtrées
+    # et qui ne nous intéressent pas (FREQ = annuelle, L_STAY = longueur du séjour, ...)
     df_capacite.drop(columns=['TOUR_MEASURE','FREQ','TIME_PERIOD','UNIT_LOC_RANKING','L_STAY'], inplace=True)
 
+    # La colonne GEO contient des codes dont on extrait le code_insee pour chaque commune 
     df_capacite['insee_code'] = df_capacite["GEO"].str.split("-").str[-1]
+    df_capacite['dept_code'] = df_capacite['insee_code'].str[:2]
 
+    # La colonne ACTIVITY encode le type d'hébergement, on va rajouter une colonne activity_type
+    # qui va donner explicitement le type d'hébergement
     dict_activity_types = {'I551' : 'Hôtel',
                            'I552' : 'Hébergement touristique',
                            'I552A' : 'Village vacances',
@@ -22,18 +39,49 @@ def transform_data_capacite(df_capacite) :
 
     df_capacite.drop(columns=['GEO'], inplace=True)
 
-    df_capacite = df_capacite[['insee_code','id_activity','activity_type','capacity']]
+    df_capacite = df_capacite[['insee_code','dept_code','id_activity','activity_type','capacity']]
+
+    # On ne va garder que les lignes des hôtels et campings, car on a pas les données des autres
+    # pour les nombres de nuitées
+    df_capacite = df_capacite[df_capacite['id_activity'].isin(['I551', 'I553'])]
 
     return df_capacite
 
 
-def transform_data_taux_hebergements(df_taux_hebergements) :
+def transform_data_nb_nuitees(df_nb_nuitees) :
+    """
+    Traitement du dataframe contenant les informations sur les nombres de nuitées de chaque
+    département. Les données obtenues sont encodées selon une nomenclature propre à l'INSEE, on va
+    donc nettoyer ces informations pour qu'elles apparaissent le plus clairement possible dans
+    le dataframe, et supprimer les colonnes inutiles.
+    De plus, on voudra récupérer des données de nombre de nuitées par mois dans chaque département,
+    pour les hôtels et les campings. Or, si pour les hôtels on les a bien, pour les campings, on a
+    seumlement les aggrégats annuels. On va donc estimer le nombre de nuitées mensuels dans les
+    campings, en supposant que la répartition au cours des mois de l'année est la même que dans les
+    hôtels : 
+        Nb_nuitees_mois_camping = Nb_nuitees_an_camping * Nb_nuitees_mois_hotel / Nb_nuitees_an_hotel
+    Args:
+        df_nb_nuitees (pd.DataFrame): DataFrame résultant de l'appel à la fonction
+    extract.extract_affluences.extract_data_nb_nuitees.
+    Returns:
+        df_nb_nuitees (pd.DataFrame): DataFrame nettoyé, où une ligne = un nombre de nuitées pour
+    un département donné, en un mois donné, sur un type d'hébergement donné.
+    """
 
-    df_taux_hebergements.drop(columns=['FREQ','TOUR_RESID','HOTEL_STA','TERRTYPO','TOUR_MEASURE','UNIT_LOC_RANKING','OBS_STATUS',
+    # On enlève les colonnes inutiles
+    df_nb_nuitees.drop(columns=['TOUR_RESID','HOTEL_STA','TERRTYPO','TOUR_MEASURE','UNIT_LOC_RANKING','OBS_STATUS',
                                        'OBS_STATUS_FR','CONF_STATUS','UNIT_MULT','DECIMALS'], inplace=True)
     
-    df_taux_hebergements['insee_code'] = df_taux_hebergements["GEO"].str.split("-").str[-1]
+    df_nb_nuitees.rename(columns={"FREQ" : "temporal_rate",
+                                "ACTIVITY" : "id_activity",
+                                "TIME_PERIOD" : "time_period",
+                                "OBS_VALUE_NIVEAU" : "nb_nights",
+                                }, inplace=True)
 
+    # On récupère le code INSEE de chaque département
+    df_nb_nuitees['dept_code'] = df_nb_nuitees["GEO"].str.split("-").str[-1]
+
+    # On rajoute une colonne activity_type comme pour les capacités
     dict_activity_types = {'I55' : 'Hébergement',
                            'I551' : 'Hôtel',
                            'I552' : 'Hébergement touristique',
@@ -41,30 +89,115 @@ def transform_data_taux_hebergements(df_taux_hebergements) :
                            'I552B' : 'Résidence de tourisme',
                            'I553' : 'Terrain de camping'}
     
-    df_taux_hebergements['activity_type'] = df_taux_hebergements['ACTIVITY'].map(dict_activity_types)
+    df_nb_nuitees['activity_type'] = df_nb_nuitees['id_activity'].map(dict_activity_types)
 
-    df_taux_hebergements.rename(columns={"ACTIVITY" : "id_activity",
-                                         "TIME_PERIOD" : "mois",
-                                        "OBS_VALUE_NIVEAU" : "accomodation_rate",
-                                }, inplace=True)
+    df_nb_nuitees.drop(columns=['GEO'], inplace=True)
 
-    df_taux_hebergements.drop(columns=['GEO'], inplace=True)
+    df_nb_nuitees = df_nb_nuitees[['temporal_rate','dept_code','time_period','id_activity','activity_type','nb_nights']]
 
-    df_taux_hebergements = df_taux_hebergements[['insee_code','mois','id_activity','activity_type','accomodation_rate']]
+    # Estimation des nombres de nuitées mensuels pour les campings
+    # On commence par séparer les données mensuelles/annuelles et campings/hôtels
+    df_hotels_month = df_nb_nuitees[(df_nb_nuitees['id_activity'] == 'I551') & (df_nb_nuitees['temporal_rate'] == 'M')].copy()
+    df_hotels_year = df_nb_nuitees[(df_nb_nuitees['id_activity'] == 'I551') & (df_nb_nuitees['temporal_rate'] == 'A')].copy()
+    df_campings_year = df_nb_nuitees[(df_nb_nuitees['id_activity'] == 'I553') & (df_nb_nuitees['temporal_rate'] == 'A')].copy()
 
-    return df_taux_hebergements
+    df_hotels_month.drop(columns=['temporal_rate'],inplace=True)
+    df_hotels_year = df_hotels_year.rename(columns={'nb_nights': 'nb_nights_hotel_year'}).drop(columns=['temporal_rate', 'id_activity'])
+    df_campings_year = df_campings_year.rename(columns={'nb_nights': 'nb_nights_camping_year'}).drop(columns=['temporal_rate', 'id_activity'])
+
+    # On récupère l'année (ici on aura que 2024)
+    df_hotels_month['year'] = df_hotels_month['time_period'].str[:4]
+    df_hotels_year['year'] = df_hotels_year['time_period'].astype(str)
+    df_campings_year['year'] = df_campings_year['time_period'].astype(str)
+
+    df_hotels_year.drop(columns=['time_period'],inplace=True)
+    df_campings_year.drop(columns=['time_period'],inplace=True)
+
+    # On merge ales données mensuelles des hôtels, annuelles des hôtels, et annuelles des campings
+    df_calc = df_hotels_month.merge(df_hotels_year, on=['dept_code','year'], how='left')
+    df_calc = df_calc.merge(df_campings_year, on=['dept_code','year'], how='left')
+
+    df_calc = df_calc[df_calc['nb_nights_hotel_year'] > 0]
+
+    # On calcule, pour chaque mois, le nb de nuitées des campings à partir du nb de nuitées annuel,
+    # du nombre de nuitées annuel des hôtels et du nombre de nuitées des hôtels dans le mois
+    df_calc['nb_nights_camping_month'] = (
+        df_calc['nb_nights_camping_year'] * df_calc['nb_nights'] / df_calc['nb_nights_hotel_year']
+                                        )
+
+    print(df_calc[['dept_code','time_period','nb_nights','nb_nights_hotel_year','nb_nights_camping_year']].head(12))
+
+    # On récupère les données mensuelles des campings ainsi calculées
+    df_campings_month = df_calc[['dept_code','time_period','nb_nights_camping_month']].copy()
+    df_campings_month['id_activity'] = 'I553'
+    df_campings_month['activity_type'] = 'Camping'
+    df_campings_month['temporal_rate'] = 'M'
+    df_campings_month = df_campings_month.rename(columns={'nb_nights_camping_month': 'nb_nights'})
+
+    df_hotels_month.drop(columns=['year'],inplace=True)
+
+    # On concatène les données mensuelles des hôtels et des campings dans df_final
+    df_final = pd.concat([
+                            df_hotels_month,
+                            df_campings_month
+                        ], ignore_index=True)
+    
+    df_final = df_final.drop(columns=['temporal_rate'])
+
+    # On s'assure qu'on a bien uniquement des hôtels et des campings
+    df_final = df_final[df_final['id_activity'].isin(['I551', 'I553'])]
+
+    return df_final
+
+
+def creation_dataframe_affluences(df_capacite, df_nb_nuitees) :
+    """
+    Création du dataframe d'affluences qui servira à l'algorithme global Voyage Voyage. Le but est 
+    de fournir, pour chaque cluster spatial (cf  transform.clusterizer.py), un nombre de 
+    touristes hébergés dans chaque type d'hébergement, pour chaque mois de l'année.
+    Le but de cette démarche sera de déterminer dans quelles zones il y a le plus et le moins de
+    touristes à chaque période de l'année.
+    Pour ce faire, on va combiner les deux dataframes de capacité d'hébergement (précis à 
+    l'échelle de la commune) et de nombre de nuitées (précis à l'échelle du département), en 
+    supposant que chaque mois, les nuitées au sein d'un département sont réparties au sein des
+    communes proportionnellement à leur capacité d'hébergement :
+        Nb_nuitees_ville = Nb_nuitees_dep * Cap_ville / Cap_dep
+    On va ensuite attribuer chaque commune au cluster auquel elle appartient,
+    et sommer les nombres de touristes obtenus au sein de chaque cluster, chaque mois.
+    Il faudra ensuite prendre garde à l'analyse que l'on peut en faire, car les clusters ne sont
+    pas tous aussi densément peuplés.
+    """
+
+    df_capacite['capacity_dept'] = df_capacite.groupby(['dept_code','id_activity'])['capacity'].transform('sum')
+
+    colonnes_capacite = ['insee_code','dept_code','id_activity','activity_type','capacity','capacity_dept']
+    colonnes_nuitees = ['dept_code','time_period','id_activity','nb_nights']
+
+
+    df_affluences = df_capacite[colonnes_capacite].merge(df_nb_nuitees[colonnes_nuitees],
+                                                        on=['dept_code', 'id_activity'],
+                                                        how='left')
+    
+    df_affluences.rename(columns={"nb_nights" : "nb_nights_dept",
+                                  "capacity" : "capacity_city"}, inplace=True)
+    
+    df_affluences['nb_nights_city'] = df_affluences['nb_nights_dept'] * 1000 * df_affluences['capacity_city'] / df_affluences['capacity_dept']
+
+    return df_affluences
 
 
 if __name__ == "__main__" :
 
-    #df_capacite = extract_affluences.extract_data_capacite()
+    df_capacite = extract_affluences.extract_data_capacite()
 
-    #df_capacite = transform_data_capacite(df_capacite)
+    df_capacite = transform_data_capacite(df_capacite)
 
-    #print(df_capacite.head())
+    print(df_capacite.head())
 
-    df_taux_hebergements = extract_affluences.extract_data_taux_remplissage()
+    df_nb_nuitees = extract_affluences.extract_data_nb_nuitees()
 
-    df_taux_hebergements = transform_data_taux_hebergements(df_taux_hebergements)
+    df_nb_nuitees = transform_data_nb_nuitees(df_nb_nuitees)
 
-    print(df_taux_hebergements.head())
+    print(df_nb_nuitees.head())
+
+    df_affluences = creation_dataframe_affluences(df_capacite, df_nb_nuitees)
