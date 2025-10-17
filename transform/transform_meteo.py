23908824cc69a37a3021531d2b1857634cc250f3
Alexandre Tonon <a.tonon65@gmail.com>
2025-10-17 17:30:24 +0200

    poursuite et fin meteo

--- transform/transform_meteo.py
+++ transform/transform_meteo.py
@@ -39,22 +39,7 @@ class TransformMeteo:
             "Latitude", "Longitude",
             "department (name)", "department (code)",
             "communes (code)", "Nom"]).unique()
-        
-
-    def create_cluster_table(self, cluster_table):
-        cluster_table["nearest_meteo_station"] = None
-
-        for cluster in cluster_table.index:
-            print(f"Processing cluster {cluster}")
-            
-            long = cluster_table.loc[cluster, 'longitude_centre']
-            lat = cluster_table.loc[cluster, 'latitude_centre']
-
-            nearest_station = self.find_nearest_station(long, lat)
-
-            cluster_table.loc[cluster, 'nearest_meteo_station'] = nearest_station
-        self.df_clusters = cluster_table
-        return self.df_clusters
+        return self.df_station_table
 
 
     def find_nearest_station(self, long, lat):
@@ -79,8 +64,21 @@ class TransformMeteo:
             df_cluster_meteo (pd.DataFrame): DataFrame où chaque ligne correspond à un cluster + mois,
                                             avec toutes les données météo de la station la plus proche
         """
+        df_cluster_table["nearest_meteo_station"] = None
+
+        for cluster in df_cluster_table.index:
+            print(f"Processing cluster {cluster}")
+
+            long = df_cluster_table.loc[cluster, 'longitude_centre']
+            lat = df_cluster_table.loc[cluster, 'latitude_centre']
+
+            nearest_station = self.find_nearest_station(long, lat)
+
+            df_cluster_table.loc[cluster, 'nearest_meteo_station'] = nearest_station
+
+
         # Merge sur le code INSEE de la station
-        df_cluster_meteo = self.df_mensuel.merge(
+        self.df_cluster_meteo = self.df_mensuel.merge(
             df_cluster_table[['nearest_meteo_station', 'code_cluster']],
             left_on='communes (code)',
             right_on='nearest_meteo_station',
@@ -88,63 +86,32 @@ class TransformMeteo:
         )
 
         # On garde la colonne code_cluster, on supprime la colonne doublon
-        df_cluster_meteo.drop(columns=['nearest_meteo_station'], inplace=True)
+        self.df_cluster_meteo.drop(columns=['nearest_meteo_station', 'Latitude', 'Longitude', 'department (code)',
+                                       'department (name)', 'communes (code)', 'Nom'], inplace=True)
 
         # Le merge conserve toutes les colonnes de df_mensuel, donc chaque mois est conservé
         # Les données sont automatiquement "par mois" grâce à la colonne 'Mois' dans df_mensuel
-        return df_cluster_meteo
+
+        self.df_cluster_meteo["ID"] = self.df_cluster_meteo["code_cluster"].astype(str) + "_" + self.df_cluster_meteo["Mois"].astype(str)
+
+        self.df_cluster_meteo.rename(columns={'Pression station': 'Meteo_Pression_station_moyenne',
+                                              'Température (°C)': 'Meteo_Temperature_moyenne',
+                                              'Précipitations dans les 24 dernières heures': 'Meteo_Precipitations_moyenne',
+                                              'Rafales sur une période': 'Meteo_Rafales_moyenne'}, inplace=True)
+
+        return self.df_cluster_meteo
 
 
     
 if __name__ == "__main__":
-    #df_meteo = pd.read_csv("data/donnees-synop-essentielles-omm.csv", sep=';')
-    #transformer = TransformMeteo(df_meteo)
-    #df_cleaned = transformer.process_data()
-    
-    #df_cluster = pd.read_csv("data/cluster_mapping.csv")
-    #cluster_table = transformer.create_cluster_table(df_cluster)
-    #cluster_table.to_csv("data/cluster_table.csv", index=False)
+    df_meteo = pd.read_csv("data/donnees-synop-essentielles-omm.csv", sep=';')
+    transformer = TransformMeteo(df_meteo)
 
-    #df_cleaned.to_csv("data/meteo_cleaned.csv", index=False)
+    df_meteo_cleaned = transformer.process_data()
+    df_meteo_cleaned.to_csv("data/meteo_cleaned.csv", index=False)
 
+    df_cluster = pd.read_csv("data/cluster_mapping.csv")
 
-
-    #test
-    df_meteo_cleaned = pd.read_csv("data/meteo_cleaned.csv")
-
-
-    df_cluster_table = pd.read_csv("data/cluster_table.csv")
-
-    print(df_cluster_table.columns)
-
-
-    def link_clusters_with_meteo(df_meteo_cleaned, df_cluster_table):
-        """
-        Associe à chaque cluster la station météo la plus proche et toutes les données météo correspondantes,
-        pour chaque mois. 
-        Met automatiquement 'code_cluster' en première colonne.
-        """
-        # Merge sur la station météo
-        df_cluster_meteo = df_meteo_cleaned.merge(
-            df_cluster_table[['nearest_meteo_station', 'code_cluster']],
-            left_on='communes (code)',
-            right_on='nearest_meteo_station',
-            how='inner'
-        )
-
-        # Supprime la colonne doublon
-        df_cluster_meteo.drop(columns=['nearest_meteo_station'], inplace=True)
-
-        # Réorganise les colonnes pour mettre 'code_cluster' en première position
-        cols = df_cluster_meteo.columns.tolist()
-        cols.remove('code_cluster')
-        cols = ['code_cluster'] + cols
-        df_cluster_meteo = df_cluster_meteo[cols]
-
-        return df_cluster_meteo
-
-
-
-    df_cluster_meteo = link_clusters_with_meteo(df_meteo_cleaned, df_cluster_table)
+    df_cluster_meteo = transformer.link_clusters_with_meteo(df_cluster)
 
     df_cluster_meteo.to_csv("data/cluster_meteo.csv", index=False)
